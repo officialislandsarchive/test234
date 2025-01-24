170685f6b2e9a790d7d787f5e0aeac94ee7f6253
officialislandsarchive <officialislandsarchive@gmail.com>
2025-01-24 15:19:21 -0600

    Update Credits.lua

--- Scripts/Lootify/Credits.lua
+++ Scripts/Lootify/Credits.lua
@@ -1,9 +1,18 @@
 return function(tab)
     local credits = tab:AddSection("Credits")
 
-    credits:AddParagraph("Executor", "Lootify - Integrated with Zynova Hub V1, providing w shit for enhanced gameplay.")
-    credits:AddParagraph("Developer", "Developed by Albert")
-    credits:AddParagraph("Contact", "For support or inquiries, reach out via Discord server discord.gg/zynovascripts.")
+    credits:AddParagraph({
+        Title = "Executor", 
+        Content = "Lootify - Integrated with Zynova Hub V1, providing w shit for enhanced gameplay.",
+    })
+    credits:AddParagraph({
+        Title = "Developer", 
+        Content = "Developed by Albert",
+    })
+    credits:AddParagraph({
+        Title = "Contact", 
+        Content = "For support or inquiries, reach out via Discord server discord.gg/zynovascripts.",
+    })
 
     credits:AddButton({
         Title = "Copy Discord Link",
